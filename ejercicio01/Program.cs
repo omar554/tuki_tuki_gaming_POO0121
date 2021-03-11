3cd485ad06f8a55deb2a6f933d50369105f48cd0
omar554 <00023620@uca.edu.sv>
2021-03-10 19:16:32 -0600

    add suma

---
+++ ejercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("Digite el numero de a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite el numero de b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"La suma de los numeros {a} + {b} es: {addnumbers( a, b)}");
+        }
+            public static int addnumbers (int a, int b)
+        {
+            return a + b;
+        } 
+   
+    }
+}
